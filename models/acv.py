e3a21f8d9746040babba0e6282da46ea2c30b5e9
Miragecore <MirageKimYs@Gmail.com>
2022-04-09 22:18:55 +0900

    add param to upsample & grid_sample

--- models/acv.py
+++ models/acv.py
@@ -198,7 +198,7 @@ class ACVNet(nn.Module):
 
             if not self.freeze_attn_weights:
 
-                cost_attention = F.upsample(att_weights, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost_attention = F.interpolate(att_weights, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear', align_corners=True)
                 cost_attention = torch.squeeze(cost_attention, 1)
                 pred_attention = F.softmax(cost_attention, dim=1)
                 pred_attention = disparity_regression(pred_attention, self.maxdisp)
@@ -209,17 +209,17 @@ class ACVNet(nn.Module):
                 cost1 = self.classif1(out1)
                 cost2 = self.classif2(out2)    
 
-                cost0 = F.upsample(cost0, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost0 = F.interpolate(cost0, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear', align_corners=True)
                 cost0 = torch.squeeze(cost0, 1)
                 pred0 = F.softmax(cost0, dim=1)
                 pred0 = disparity_regression(pred0, self.maxdisp)    
 
-                cost1 = F.upsample(cost1, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost1 = F.interpolate(cost1, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear', align_corners=True)
                 cost1 = torch.squeeze(cost1, 1)
                 pred1 = F.softmax(cost1, dim=1)
                 pred1 = disparity_regression(pred1, self.maxdisp)    
 
-                cost2 = F.upsample(cost2, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost2 = F.interpolate(cost2, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear', aligner_corners=True)
                 cost2 = torch.squeeze(cost2, 1)
                 pred2 = F.softmax(cost2, dim=1)
                 pred2 = disparity_regression(pred2, self.maxdisp)
@@ -233,14 +233,14 @@ class ACVNet(nn.Module):
 
             if self.attn_weights_only:
 
-                cost_attention = F.upsample(att_weights, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost_attention = F.interpolate(att_weights, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear', aligner_corners=True)
                 cost_attention = torch.squeeze(cost_attention, 1)
                 pred_attention = F.softmax(cost_attention, dim=1)
                 pred_attention = disparity_regression(pred_attention, self.maxdisp)
                 return [pred_attention]
 
             cost2 = self.classif2(out2)
-            cost2 = F.upsample(cost2, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+            cost2 = F.interpolate(cost2, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear', aligner_corners=True)
             cost2 = torch.squeeze(cost2, 1)
             pred2 = F.softmax(cost2, dim=1)
             pred2 = disparity_regression(pred2, self.maxdisp)
